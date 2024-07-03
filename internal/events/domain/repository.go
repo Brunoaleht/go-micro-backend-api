7e0d9e9b87b6e7d22f3f7cd0aeafcb6afd0e0c57
Bruno Alehtxandre <brunoaleh1@gmail.com>
2024-07-03 20:25:22 -0300

    Feature de infra incompleta sem fectore e sem partner 2

--- internal/events/domain/repository.go
+++ internal/events/domain/repository.go
@@ -4,7 +4,7 @@ type EventRepository interface {
 	ListEvents() ([]Event, error)
 	GetEventByID(eventId string) (*Event, error)
 	FindSpotsEventID(eventId string) ([]Spot, error)
-	FindSpotByNames(eventId, spotNames string) ([]Spot, error)
+	FindSpotByName(eventId, spotNames string) (*Spot, error)
 	CreateSpot(spot *Spot) error
 	CreateTicket(ticket *Ticket) error
 	ReserveSpot(spotId, ticketId string) error
--- internal/events/usecase/buy_tickets.go
+++ internal/events/usecase/buy_tickets.go
@@ -7,7 +7,7 @@ import (
 type BuyTicketsInputDto struct {
 	EventID string `json:"event_id"`
 	Spots []string `json:"spots"`
-	TicketKind string `json:"ticket_kind"`
+	TicketKind string `json:"ticket_status"`
 	CardHash string `json:"card_hash"`
 	Email string `json:"email"`
 }
@@ -20,10 +20,86 @@ type TicketDto struct {
 	ID string `json:"id"`
 	SpotID string `json:"spot_id"`
 	EventID string `json:"event_id"`
-	TicketKind string `json:"ticket_kind"`
+	TicketKind string `json:"ticket_status"`
 	Price float64 `json:"price"`
 }
 
 type BuyTicketsUseCase struct {
 	repo domain.EventRepository
+	partnerFactory service.PartnerFactory
+}
+
+func NewBuyTicketsUseCase(repo domain.EventRepository, partnerFactory service.PartnerFactory) *BuyTicketsUseCase {
+	return &BuyTicketsUseCase{
+		repo: repo, 
+		partnerFactory: partnerFactory,
+	}
+}
+
+func (uc *BuyTicketsUseCase) Execute(input BuyTicketsInputDto) (*BuyTicketsOutputDto, error) {
+	event, err := uc.repo.GetEventByID(input.EventID)
+	if err != nil {
+		return nil, err
+	}
+
+	//criar a solicitação de reserva
+	reserver := &service.ReservationRequest{
+		EventID: input.EventID,
+		Spots: input.Spots,
+		TicketKind: input.TicketKind,
+		CardHash: input.CardHash,
+		Email: input.Email,
+		
+	}
+
+	//Obtendo o parceiro
+	partnerService, err := uc.partnerFactory.GetPartner(event.PartnerID)
+	if err != nil {
+		return nil, err
+	}
+
+	//Reservar os tickets usando o serviço do parceiro
+	reservationResponse, err := partnerService.MakeReservation(reserver)
+	if err != nil {
+		return nil, err
+	}
+
+	//salvando os tickets no banco de dados
+	tickets := make([]domain.Ticket, len(reservationResponse))
+	for i, reservation := range reservationResponse {
+		spot, err := uc.repo.FindSpotByName(event.ID, reservation.Spot)
+		if err != nil {
+			return nil, err
+		}
+
+		ticket, err := domain.CreatedNewTicket(event, spot, domain.TicketStatus(input.TicketKind))
+		if err != nil {
+			return nil, err
+		}
+
+		err = uc.repo.CreateTicket(ticket)
+		if err != nil {
+			return nil, err
+		}
+
+		spot.ReserveSpot(ticket.ID)
+		err = uc.repo.ReserveSpot(spot.ID, ticket.ID)
+		if err != nil {
+			return nil, err
+		}
+
+		tickets[i] = *ticket
+	}
+
+	ticketDto := make([]TicketDto, len(tickets))
+	for i, ticket := range tickets {
+		ticketDto[i] = TicketDto{
+			ID: ticket.ID,
+			SpotID: ticket.Spot.ID,
+			TicketKind: string(ticket.TicketKind),
+			Price: ticket.Price,
+		}
+	}
+	
+	return &BuyTicketsOutputDto{Tickets: ticketDto}, nil
 }
